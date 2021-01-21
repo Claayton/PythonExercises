bfeb880736a5e794d29d6b739c73da9d9f852821
Clayton Garcia da Silva <76628101+Claayton@users.noreply.github.com>
2021-01-21 12:03:59 -0300

    Exercício completo!

--- ex013.2.py
+++ ex013.2.py
@@ -1,2 +1,5 @@
 #Ex013.2
 salary = float(input("Type the employee's salary: "))
+increase = salary * 15 / 100
+print(f"If the employee's salary was R$: {salary:.2f}")
+print(f'with a 15% increase, you will receive R$: {salary + increase:.2f}')

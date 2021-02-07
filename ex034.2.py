ac895fc2d03c9a4dc5cbe5a503833f225b36388e
Clayton <claaytongarcia@gmail.com>
2021-02-07 18:33:33 -0300

    Editados com enunciado em ingles

--- ex034.2.py
+++ ex034.2.py
@@ -1,5 +1,11 @@
 # Ex034.2
 """Write a program that asks for an employer's salary and calculates the amount of their increase,
-For salaries greater than R$ 1200.00, calculate an increase of 10%
+For salaries greater than R$ 1250.00, calculate an increase of 10%
 For less than of equals, the increases is 15%"""
 
+salary = float(input("What is the employee' salary? "))
+if salary > 1250:
+    increase = 10
+else:
+    increase = 15
+print(f'Whoever received R$: {salary:.2f}, starts to receive R$: {salary + ((salary * increase) / 100):.2f}')
---
+++ ex035.2.py
@@ -0,0 +1,16 @@
+# Ex035.2
+"""Develop a program that reads the length of three lines, and tell the user whether or not they can form a triangle"""
+
+print('=-' * 15)
+print(f'{"TRIANGLE ANALYZING":-^30}')
+print('=-' * 15)
+first = float(input('First segment: '))
+second = float(input('Second segment: '))
+third = float(input('third segment: '))
+list = [first, second, third]
+list = sorted(list)
+print(list)
+if list[0] + list[1] > list[2]:
+    print('The above segments CAN form a triangle')
+else:
+    print('The above segments CANNOT form a triangle')

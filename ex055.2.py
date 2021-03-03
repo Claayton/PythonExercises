98d1575f0583721ee21c566fa2d4b4df0b0e8784
Clayton Garcia da Silva <76628101+Claayton@users.noreply.github.com>
2021-03-02 23:37:44 -0300

    Updated for fhsl

--- ex055.2.py
+++ ex055.2.py
@@ -6,15 +6,15 @@ highest = 0
 lowest = 0
 
 for p in range(1, 6):
-    weight = float(input(f'What is the weight of the {p}° person?: '))
+  weight = float(input(f'What is the weight of the {p}° person?: '))
+  if p == 1:
     highest_weight = weight
     lowest_weight = weight
-    if weight > highest_weight:
-        highest += 1
-        highest_weight = weight
-    if weight < lowest_weight:
-        lowest += 1
-        lowest_weight = weight
+  if weight > highest_weight:
+    highest = p
+    highest_weight = weight
+  if weight < lowest_weight:
+    lowest = p
+    lowest_weight = weight
 print(f'The highest weight is the {highest}° person with {highest_weight:.2f}Kg')
 print(f'The lowest weight is the {lowest}° person with {lowest_weight:.2f}Kg')
-# não terminado
